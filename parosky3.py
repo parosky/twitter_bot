8e32a9539af1e5cc7c6bfb9f13259020680b1644
parosky <hkato@hkato-desktop.(none)>
2013-03-28 16:46:42 +0900

    parosky3

---
+++ parosky3.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import settings
+import psbot
+import urllib
+import re
+import os
+import sys
+import pickle
+import json
+import xml.sax.saxutils
+import sqlalchemy
+import tweepy
+import random
+import datetime
+
+class Parosky3(psbot.BaseTwitterBot):
+    def __init__(self):
+        screen_name = "parosky3"
+        consumer_key = settings.user_apikey[screen_name]["consumer_key"]
+        consumer_secret = settings.user_apikey[screen_name]["consumer_secret"]
+        access_token = settings.user_apikey[screen_name]["access_token"]
+        access_token_secret = settings.user_apikey[screen_name]["access_token_secret"]
+        
+        #self.append_calllist(self.favorite_replies, 60*11)
+        #self.append_calllist(self.favorite_retweets, 23)
+        #self.append_calllist(self.update_database, 60*13)
+        #self.append_calllist(self.follow_back, 60*7)
+        #self.append_calllist(self.follow, 2)
+        self.append_calllist(self.post, 6)
+        
+        psbot.BaseTwitterBot.__init__(self, screen_name, consumer_key, consumer_secret, access_token, access_token_secret)
+    
+    # post to twitter
+    def post(self):
+        tweets_per_hour = pickle.load(open('parosky3_tweets_per_hour.p'))
+        rand = random.random()
+        hour = datetime.datetime.now().hour
+        if rand >= tweets_per_hour[hour] / 10:
+            return
+        
+        tweets = pickle.load(open('parosky3_tweets.p'))
+        rand = random.random()
+        for t in tweets:
+            if rand <= t[2]:
+                tweet = t[1]
+                break
+        print tweet
+        self.api.update_status(tweet)
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(sys.argv[0]) or '.')
+    parosky3 = Parosky3()
+    if len(sys.argv) > 1:
+        uid = sys.argv[1]
+        parosky3.make_follow_list_from_followers(uid, ["parosky0"])
+    else:
+        parosky3.run()
+

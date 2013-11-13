697fd4450b0844af87e0364f1582f9b256dbfdb5
parosky <hkato@hkato-Dell-System-Inspiron-N411Z.(none)>
2013-11-13 14:07:29 +0900

    modified parosky1 not to use favster

--- parosky1.py
+++ parosky1.py
@@ -20,15 +20,15 @@ class Parosky1(psbot.BaseTwitterBot):
         consumer_secret = settings.user_apikey[screen_name]["consumer_secret"]
         access_token = settings.user_apikey[screen_name]["access_token"]
         access_token_secret = settings.user_apikey[screen_name]["access_token_secret"]
-        
+
         self.append_calllist(self.favorite_replies, 60*11)
         self.append_calllist(self.update_database, 60*13)
         self.append_calllist(self.follow_back, 60*7)
         self.append_calllist(self.follow, 5)
         self.append_calllist(self.post, 2)
-        
+
         psbot.BaseTwitterBot.__init__(self, screen_name, consumer_key, consumer_secret, access_token, access_token_secret)
-    
+
     # post to twitter
     # repost parosky0's post which is 3 or more favs/RTs
     def post(self):
@@ -41,42 +41,21 @@ class Parosky1(psbot.BaseTwitterBot):
 
         recent_id = int(post_recentid.value)
         print recent_id
-        
-        # get favstar html
-        url = 'http://favstar.fm/users/parosky0/recent'
-        favstring = urllib.urlopen(url).read()
 
-        # extract posts and favs/RTs
-        favs = []
-        p = re.compile(r"data-model='.*?'")
-        for line in favstring.splitlines():
-            if "'fs-tweet'" in line:
-                line = p.search(line).group(0)
-                line = line.replace("data-model=", "")
-                line = line.strip("'")
-                tw = json.loads(line)
-                favs.append({})
-                favs[-1]['id'] = int(tw["tweet_id"])
-                favs[-1]['count'] = 0
-            if "'fs-tweet-meta fs-sunken-panel'" in line:
-                line = p.search(line).group(0)
-                line = line.replace("data-model=", "")
-                line = line.strip("'")
-                tw = json.loads(line)
-                for t in tw:
-                    favs[-1]['count'] += int(t["total"])
+        tweets = self.api.user_timeline('parosky0')
 
         # post
-        for fav in favs:
-            if (fav['id'] > recent_id) and (fav['count'] >= 3):
-                text = self.api.get_status(fav['id']).text
+        for tweet in tweets:
+            if ((tweet.id > recent_id) and
+                    (tweet.favorite_count+tweet.retweet_count >= 3)):
+                text = tweet.text
                 if '@' in text:
                     continue
                 try:
                     self.api.update_status(text)
                 except tweepy.TweepError:
                     return
-                post_recentid.value = str(max(recent_id, fav['id']))
+                post_recentid.value = str(max(recent_id, tweet.id))
                 session.commit()
                 session.close()
                 return
